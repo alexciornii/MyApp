1c6c75d2b7685b2489e38224fd4fd0d7d248a088
Alex Ciornii <c_aciornii@zipwhip.com>
2018-11-14 19:49:15 +0200

    first commit

---
+++ src/main/java/com/jarvis/auth/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.jarvis.auth.repository;
+
+import com.jarvis.auth.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByRole(String role);
+}
---
+++ src/main/java/com/jarvis/auth/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.jarvis.auth.repository;
+
+import com.jarvis.auth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
